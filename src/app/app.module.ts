97fd1d2377ede8a555fdb10d7a6f71c7ee45ff59
Sourav Kundu <souravkundu5@gmail.com>
2021-03-19 10:45:34 +0530

    First commit to webcam project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WebcamComponent } from './webcam/webcam.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WebcamComponent
   ],
   imports: [
     BrowserModule
